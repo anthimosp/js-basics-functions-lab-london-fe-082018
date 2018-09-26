4e1b8043253c72a9dd2c3198442e3a75961fa336
avid-rubyist-1973 <anthimos.pavlidis@wework.com>
2018-09-26 16:53:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) { 
+  return Math.abs(blocks-42);
+}
+
+function distanceFromHqInFeet(blocks) {
+  return distanceFromHqInBlocks(blocks)*264;
+}
+
+function distanceTravelledInFeet(feet) {
+  return distanceFromHqInFeet(feet)*2;
+}
